2d34b5476793c6340e0e329e80789992d9d760d4
Gonzalo Astargo <g.astargo84@gmail.com>
2024-08-29 20:24:21 -0400

    CATEGORY INTO PRODUCT FORM

--- src/pages/components/ProductForm.js
+++ src/pages/components/ProductForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -11,17 +11,25 @@ export default function ProductForm({
   description: existingDescription,
   price: existingPrice,
   images: existingImages,
+  category: existingCategory,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
+  const [category, setCategory] = useState(existingCategory || "");
   const [price, setPrice] = useState(existingPrice || "");
   const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
+  const [categories, setCategories] = useState([]);
   const router = useRouter();
+  useEffect(() => {
+    axios.get("/api/categories").then(result => {
+      setCategories(result.data);
+    });
+  }, []);
   async function saveProduct(ev) {
     ev.preventDefault();
-    const data = { title, description, price,images };
+    const data = { title, description, price, images, category };
     if (_id) {
       await axios.put("/api/products", { ...data, _id });
     } else {
@@ -41,13 +49,13 @@ export default function ProductForm({
         data.append("file", file);
       }
       const res = await axios.post("/api/upload", data);
-      setImages(oldImages => {
-        return [...oldImages,...res.data.links];
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
       });
       setIsUploading(false);
     }
   }
-  function updateImagesOrder(images){
+  function updateImagesOrder(images) {
     setImages(images);
   }
   return (
@@ -59,22 +67,33 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Categoria</label>
+      <select
+        value={category}
+        onChange={ev => setCategory(ev.target.value)}
+      >
+        <option value="">Sin categoría</option>
+        {categories.length > 0 && categories.map(c => (
+          <option value={c._id}>{c.name}</option>
+          ))}
+      </select>
       <label>Imagenes</label>
       <div className="mb-2 flex flex-wrap gap-2">
-        <ReactSortable 
-        list={images} 
-        setList={updateImagesOrder}
-        className="flex flex-wrap gap-2"
+        <ReactSortable
+          list={images}
+          setList={updateImagesOrder}
+          className="flex flex-wrap gap-2"
         >
-        {!!images?.length && images.map(link =>(
-          <div key={link} className="h-24">
-            <img className="rounded-lg" src={link} alt="" />
-          </div>
-        ))}
+          {!!images?.length &&
+            images.map((link) => (
+              <div key={link} className="h-24">
+                <img className="rounded-lg" src={link} alt="" />
+              </div>
+            ))}
         </ReactSortable>
-        {isUploading &&(
+        {isUploading && (
           <div className="w-24 h-24 border flex text-center items-center justify-center text-sm text-gray-500 font-semibold gap-2 rounded-lg bg-gray-200 attach">
-            <Spinner/>
+            <Spinner />
           </div>
         )}
         <label className="w-24 h-24 border flex text-center items-center justify-center text-sm text-gray-500 font-semibold gap-1 rounded-lg bg-gray-200 attach-button">
