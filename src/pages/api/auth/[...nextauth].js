58c936351d8f207e6423789242d0f5da1540c29e
xe1k <g.astargo84@gmail.com>
2024-09-03 10:20:25 -0400

    session function, validate admin

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,10 +1,12 @@
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import client from '../../../lib/mongodb'
 import GoogleProvider from 'next-auth/providers/google'
 
 
-export default NextAuth({
+const adminEmails = ['xeikonly.wow@gmail.com'];
+
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
@@ -13,4 +15,30 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(client),
-})
+  callbacks: {
+    session: async ({ session, token, user }) => {
+      console.log('Session object:', session);
+      console.log('User object:', user);
+      console.log('Token object:', token);
+
+      if (session?.user?.email && adminEmails.includes(session.user.email)) {
+        return session;
+      } else {
+        console.log('User email is not in adminEmails.');
+        return null;
+      }
+    },
+  }
+
+}
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'No es administrador'
+  }
+}
--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -11,6 +11,12 @@ export default function Nav() {
     inactiveLink + " bg-white font-bold text-[#444444] rounded-l-lg";
   const router = useRouter();
   const { pathname } = router;
+
+  async function logout() {
+    await router.push('/');
+    await signOut();
+  }
+
   return (
     <aside className="text-white p-4 pr-0">
       <Link href={"/"} className="flex h-16 p-3 mb-2">
@@ -127,11 +133,11 @@ export default function Nav() {
         </Link>
         <button
           className={inactiveLink}
-          onClick={ () => signOut()}
+          onClick={logout}
         >
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-</svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+          </svg>
 
           Cerrar sesión
         </button>
