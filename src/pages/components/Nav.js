226e009be939962ad1b0e6c6aa23cd9a25bba92d
xe1k <g.astargo84@gmail.com>
2024-09-03 09:22:19 -0400

    logout function

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -3,6 +3,7 @@ import Logo from "../../../public/LOGO.png";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
 
 export default function Nav() {
   const inactiveLink = "flex gap-2 text-xl py-2 ";
@@ -124,6 +125,16 @@ export default function Nav() {
           </svg>
           Configuración
         </Link>
+        <button
+          className={inactiveLink}
+          onClick={ () => signOut()}
+        >
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+</svg>
+
+          Cerrar sesión
+        </button>
       </nav>
     </aside>
   );
