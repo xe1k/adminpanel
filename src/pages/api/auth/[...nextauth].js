623684fa73a88da8aadd765ed9e4069ef6fbd494
Gonzalo Astargo <g.astargo84@gmail.com>
2024-09-13 10:57:35 -0300

    login fixed

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,36 +1,49 @@
-import NextAuth, { getServerSession } from 'next-auth'
-import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import client from '../../../lib/mongodb'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth, { getServerSession } from 'next-auth';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import client from '../../../lib/mongodb';
+import GoogleProvider from 'next-auth/providers/google';
 
-
-const adminEmails = ['xeikonly.wow@gmail.com'];
+const adminEmails = ['g.astargo84@gmail.com','xeikonly.wow@gmail.com'];
 
 export const authOptions = {
   providers: [
-    // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(client),
   callbacks: {
-    session: async ({ session, token, user }) => {
-      console.log('Session object:', session);
-      console.log('User object:', user);
-      console.log('Token object:', token);
-
-      if (session?.user?.email && adminEmails.includes(session.user.email)) {
-        return session;
-      } else {
-        console.log('User email is not in adminEmails.');
-        return null;
+    async signIn({ user, account, profile }) {
+      // Verifica si el correo del usuario está en adminEmails
+      if (!adminEmails.includes(user.email)) {
+        console.log(`Acceso denegado para ${user.email}`);
+        return false; // Deniega el inicio de sesión
       }
-    },
-  }
 
-}
+      const existingUser = await client.db().collection('users').findOne({ email: user.email });
+  
+      if (existingUser && existingUser.provider !== account.provider) {
+        // Si el usuario ya existe pero con otro proveedor, actualiza el proveedor
+        console.log('Unificando cuenta con diferentes proveedores');
+        await client.db().collection('users').updateOne(
+          { email: user.email },
+          { $set: { provider: account.provider } }
+        );
+      }
+  
+      return true; // Permite que el inicio de sesión continúe si es administrador
+    },
+    async session({ session, token, user }) {
+      if (session?.user?.email && adminEmails.includes(session.user.email)) {
+        session.isAdmin = true; // Marca el usuario como administrador
+      } else {
+        session.isAdmin = false; // No es administrador
+      }
+      return session;
+    },
+  },
+};
 
 export default NextAuth(authOptions);
 
@@ -39,6 +52,6 @@ export async function isAdminRequest(req, res) {
   if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
     res.end();
-    throw 'No es administrador'
+    throw 'No es administrador';
   }
 }
